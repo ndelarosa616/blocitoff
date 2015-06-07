39bc4afda046c671d2efea1ced8a73a05824dd40
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-06-07 12:26:35 -0400

    added faker and seeded database with user/item information

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create Items
+20.times do
+  Item.create!(
+    user: users.sample,
+    name: Faker::Lorem.sentence
+    )  
+end
+posts = Item.all 
+
+user = User.new(
+  name: "Nicole De La Rosa",
+  email: 'nicoledelarosa616@gmail.com',
+  password: 'monkey324'
+  )
+user.skip_confirmation!
+user.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"
+
+
+
+
