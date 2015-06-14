2acd213fd37ce485c88ac401a766d27c26d6d853
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-06-14 11:22:37 -0400

    added project manager creates todo passing rspec

--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -5,11 +5,27 @@ feature 'Project manager creates TODO' do
   before do
     @user = create(:user)
     @item = create(:item)
+
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button 'Sign in'
   end
+
 
   scenario 'Successfully' do
     user = @user
     visit user_path(user)
-    fill_in 'new_item', with: @item.name
+    fill_in 'item_name', with: @item.name
+    click_button 'Add'
+
+    expect(page).to have_content(@item.name)
   end  
+
+  scenario 'Unsucessfully' do
+    fill_in 'item_name', with: ''
+    click_button 'Add'
+
+    expect(page).to have_content('Error saving ToDo - please try again.')
+  end
 end
