c2a70e8ab0aa976a0016507420d0a4a7677f0695
Nicole De La Rosa <nicoledelarosa616@gmail.com>
2015-07-08 23:55:07 -0400

    added functionality to be able to update an item from the command line.

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,19 @@
 class Api::ItemsController < ApiController
-  skip_before_action :authenticate, only: [:create, :destroy]
+  skip_before_action :authenticate, only: [:create, :destroy, :update]
 
   def create
     item = Item.new(item_params)
 
     if item.save
+      render json: item
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
       render json: item
     else
       render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
